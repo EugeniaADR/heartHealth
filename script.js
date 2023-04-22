146d2ca030b254c693253c8f50a822cbbcae26f6
eugeniaAdr <eugenia.adr@gmail.com>
2023-04-21 19:12:39 -0500

    update calculator

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const button = document.querySelector("#btn");
 button.addEventListener("click", calculateSatFats);
 
 function calculateSatFats(e) {
-  e.preventDefault(); // do not reload the page automatically
-  let calories = document.querySelector("#dailyCalories").value;
+    e.preventDefault()// do not reload the page automatically
+    let calories = document.querySelector("#dailyCalories").value;
+   
 
-  if (calories === "") {
-    alert("Error!");
-  }
+    if (calories === "") {
+        alert("Error!")
+    }
 
-  let grPer5caloriesFromSatFats = (calories * 0.05 * 13) / 120; // 5% of calories from saturated fat, 13/120 коэффицент gr per cal
 
-  let grPer6caloriesFromSatFats = (calories * 0.06 * 13) / 120; // 6% of calories from saturated fat, 13/120 коэффицент gr per cal
+    let calFromSatFats5 = calories * 0.05 // 5% of calories from saturated fat,
 
-  document.querySelector(
-    "#totalAmountOfSatFats"
-  ).textContent = `If you need about ${calories} calories a day, the recommended amount of saturated fat for you is ${grPer5caloriesFromSatFats.toFixed()} to ${grPer6caloriesFromSatFats.toFixed()} grams per day.`;
+    let calFromSatFats6 = calories * 0.06 // 6% of calories from saturated fat,
+        
+    let gramsOfSatFats5 = calFromSatFats5 * 13 / 120; //  13/120 коэффицент gr per cal
+
+    let gramsOfSatFats6 = calFromSatFats6 * 13 / 120; // 13/120 коэффицент gr per cal
+
+    document.querySelector("#totalAmountOfSatFats").textContent = `If you need about ${calories} calories a day, 
+    no more than ${calFromSatFats5.toFixed()} - ${calFromSatFats6.toFixed()} of them should come from saturated fat.
+   That's about ${gramsOfSatFats5.toFixed()} - ${gramsOfSatFats6.toFixed()} grams of saturated fat per day`
 }
 
 const btnCalculate = document.querySelector("#calculate");
